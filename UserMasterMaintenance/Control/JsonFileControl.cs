023d4382cb5f7545c0bc127708c735716c780cda
koda-y <koda-y@evoltech.co.jp>
2020-03-19 17:41:17 +0900

    JsonControl作成途中

--- UserMasterMaintenance/Control/JsonFileControl.cs
+++ UserMasterMaintenance/Control/JsonFileControl.cs
@@ -8,5 +8,38 @@ namespace UserMasterMaintenance.Control
 {
 	class JsonFileControl
 	{
+		/// <summary>
+		/// ユーザーリスト
+		/// </summary>
+		public List<Model.User> Users { get; set; }
+
+		/// <summary>
+		/// 部門リスト
+		/// </summary>
+		public List<Model.Department> Departments { get; set; }
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public JsonFileControl()
+		{
+			Selialize();
+		}
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		public void Selialize()
+		{
+			
+		}
+
+		/// <summary>
+		/// デシリアライズ
+		/// </summary>
+		public void Desilialize()
+		{
+
+		}
 	}
 }
