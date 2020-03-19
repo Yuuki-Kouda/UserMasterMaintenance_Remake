9ae697ab85fccba7ee30d2330fa1183a8ed647f0
koda-y <koda-y@evoltech.co.jp>
2020-03-19 17:41:29 +0900

    デザイン作成

--- UserMasterMaintenance/View/EditForm.Designer.cs
+++ UserMasterMaintenance/View/EditForm.Designer.cs
@@ -28,12 +28,195 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.IdLabel = new System.Windows.Forms.Label();
+			this.NameLabel = new System.Windows.Forms.Label();
+			this.AgeLabel = new System.Windows.Forms.Label();
+			this.GenderLabel = new System.Windows.Forms.Label();
+			this.DepartmentLabel = new System.Windows.Forms.Label();
+			this.OKButton = new System.Windows.Forms.Button();
+			this.CancelButton = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.MenRadioButton = new System.Windows.Forms.RadioButton();
+			this.WomenRadioButton = new System.Windows.Forms.RadioButton();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// IdLabel
+			// 
+			this.IdLabel.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.IdLabel.Location = new System.Drawing.Point(30, 55);
+			this.IdLabel.Name = "IdLabel";
+			this.IdLabel.Size = new System.Drawing.Size(38, 23);
+			this.IdLabel.TabIndex = 0;
+			this.IdLabel.Text = "ID";
+			this.IdLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// NameLabel
+			// 
+			this.NameLabel.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.NameLabel.Location = new System.Drawing.Point(30, 114);
+			this.NameLabel.Name = "NameLabel";
+			this.NameLabel.Size = new System.Drawing.Size(58, 23);
+			this.NameLabel.TabIndex = 1;
+			this.NameLabel.Text = "名前";
+			this.NameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// AgeLabel
+			// 
+			this.AgeLabel.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.AgeLabel.Location = new System.Drawing.Point(30, 180);
+			this.AgeLabel.Name = "AgeLabel";
+			this.AgeLabel.Size = new System.Drawing.Size(58, 23);
+			this.AgeLabel.TabIndex = 2;
+			this.AgeLabel.Text = "年齢";
+			this.AgeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// GenderLabel
+			// 
+			this.GenderLabel.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.GenderLabel.Location = new System.Drawing.Point(30, 251);
+			this.GenderLabel.Name = "GenderLabel";
+			this.GenderLabel.Size = new System.Drawing.Size(58, 23);
+			this.GenderLabel.TabIndex = 3;
+			this.GenderLabel.Text = "性別";
+			this.GenderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// DepartmentLabel
+			// 
+			this.DepartmentLabel.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.DepartmentLabel.Location = new System.Drawing.Point(30, 328);
+			this.DepartmentLabel.Name = "DepartmentLabel";
+			this.DepartmentLabel.Size = new System.Drawing.Size(58, 23);
+			this.DepartmentLabel.TabIndex = 4;
+			this.DepartmentLabel.Text = "所属";
+			this.DepartmentLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// OKButton
+			// 
+			this.OKButton.Location = new System.Drawing.Point(157, 388);
+			this.OKButton.Name = "OKButton";
+			this.OKButton.Size = new System.Drawing.Size(90, 50);
+			this.OKButton.TabIndex = 5;
+			this.OKButton.Text = "OK";
+			this.OKButton.UseVisualStyleBackColor = true;
+			// 
+			// CancelButton
+			// 
+			this.CancelButton.Location = new System.Drawing.Point(256, 388);
+			this.CancelButton.Name = "CancelButton";
+			this.CancelButton.Size = new System.Drawing.Size(90, 50);
+			this.CancelButton.TabIndex = 6;
+			this.CancelButton.Text = "キャンセル";
+			this.CancelButton.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox1.Location = new System.Drawing.Point(88, 55);
+			this.textBox1.MaxLength = 4;
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(63, 23);
+			this.textBox1.TabIndex = 7;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox2.Location = new System.Drawing.Point(88, 114);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(258, 23);
+			this.textBox2.TabIndex = 8;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox3.Location = new System.Drawing.Point(88, 180);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(63, 23);
+			this.textBox3.TabIndex = 9;
+			// 
+			// MenRadioButton
+			// 
+			this.MenRadioButton.AutoEllipsis = true;
+			this.MenRadioButton.AutoSize = true;
+			this.MenRadioButton.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.MenRadioButton.Location = new System.Drawing.Point(112, 251);
+			this.MenRadioButton.Name = "MenRadioButton";
+			this.MenRadioButton.Size = new System.Drawing.Size(69, 24);
+			this.MenRadioButton.TabIndex = 12;
+			this.MenRadioButton.TabStop = true;
+			this.MenRadioButton.Text = "男性";
+			this.MenRadioButton.UseVisualStyleBackColor = true;
+			// 
+			// WomenRadioButton
+			// 
+			this.WomenRadioButton.AutoSize = true;
+			this.WomenRadioButton.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.WomenRadioButton.Location = new System.Drawing.Point(201, 251);
+			this.WomenRadioButton.Name = "WomenRadioButton";
+			this.WomenRadioButton.Size = new System.Drawing.Size(69, 24);
+			this.WomenRadioButton.TabIndex = 13;
+			this.WomenRadioButton.TabStop = true;
+			this.WomenRadioButton.Text = "女性";
+			this.WomenRadioButton.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(88, 323);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(258, 28);
+			this.comboBox1.TabIndex = 14;
+			// 
+			// EditForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "EditForm";
+			this.ClientSize = new System.Drawing.Size(358, 450);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.WomenRadioButton);
+			this.Controls.Add(this.MenRadioButton);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.CancelButton);
+			this.Controls.Add(this.OKButton);
+			this.Controls.Add(this.DepartmentLabel);
+			this.Controls.Add(this.GenderLabel);
+			this.Controls.Add(this.AgeLabel);
+			this.Controls.Add(this.NameLabel);
+			this.Controls.Add(this.IdLabel);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(374, 489);
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(374, 489);
+			this.Name = "EditForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "UserMasterMaintenance";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label IdLabel;
+		private System.Windows.Forms.Label NameLabel;
+		private System.Windows.Forms.Label AgeLabel;
+		private System.Windows.Forms.Label GenderLabel;
+		private System.Windows.Forms.Label DepartmentLabel;
+		private System.Windows.Forms.Button OKButton;
+		private System.Windows.Forms.Button CancelButton;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.RadioButton MenRadioButton;
+		private System.Windows.Forms.RadioButton WomenRadioButton;
+		private System.Windows.Forms.ComboBox comboBox1;
 	}
 }
--- UserMasterMaintenance/View/ListForm.Designer.cs
+++ UserMasterMaintenance/View/ListForm.Designer.cs
@@ -28,13 +28,136 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.RegisterButton = new System.Windows.Forms.Button();
+			this.UpdateButton = new System.Windows.Forms.Button();
+			this.DeleteButton = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.UserCheck = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+			this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Gender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Department = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// RegisterButton
+			// 
+			this.RegisterButton.Location = new System.Drawing.Point(12, 12);
+			this.RegisterButton.Name = "RegisterButton";
+			this.RegisterButton.Size = new System.Drawing.Size(94, 48);
+			this.RegisterButton.TabIndex = 0;
+			this.RegisterButton.Text = "登録";
+			this.RegisterButton.UseVisualStyleBackColor = true;
+			// 
+			// UpdateButton
+			// 
+			this.UpdateButton.Location = new System.Drawing.Point(112, 12);
+			this.UpdateButton.Name = "UpdateButton";
+			this.UpdateButton.Size = new System.Drawing.Size(94, 48);
+			this.UpdateButton.TabIndex = 1;
+			this.UpdateButton.Text = "更新";
+			this.UpdateButton.UseVisualStyleBackColor = true;
+			// 
+			// DeleteButton
+			// 
+			this.DeleteButton.Location = new System.Drawing.Point(212, 12);
+			this.DeleteButton.Name = "DeleteButton";
+			this.DeleteButton.Size = new System.Drawing.Size(94, 48);
+			this.DeleteButton.TabIndex = 2;
+			this.DeleteButton.Text = "削除";
+			this.DeleteButton.UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserCheck,
+            this.ID,
+            this.Name,
+            this.Age,
+            this.Gender,
+            this.Department});
+			this.dataGridView1.Location = new System.Drawing.Point(12, 66);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 21;
+			this.dataGridView1.Size = new System.Drawing.Size(776, 372);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// UserCheck
+			// 
+			this.UserCheck.HeaderText = "";
+			this.UserCheck.Name = "UserCheck";
+			this.UserCheck.Width = 50;
+			// 
+			// ID
+			// 
+			this.ID.HeaderText = "ID";
+			this.ID.Name = "ID";
+			this.ID.ReadOnly = true;
+			this.ID.Width = 136;
+			// 
+			// Name
+			// 
+			this.Name.HeaderText = "名前";
+			this.Name.Name = "Name";
+			this.Name.ReadOnly = true;
+			this.Name.Width = 136;
+			// 
+			// Age
+			// 
+			this.Age.HeaderText = "年齢";
+			this.Age.Name = "Age";
+			this.Age.ReadOnly = true;
+			this.Age.Width = 136;
+			// 
+			// Gender
+			// 
+			this.Gender.HeaderText = "性別";
+			this.Gender.Name = "Gender";
+			this.Gender.ReadOnly = true;
+			this.Gender.Width = 136;
+			// 
+			// Department
+			// 
+			this.Department.HeaderText = "所属";
+			this.Department.Name = "Department";
+			this.Department.ReadOnly = true;
+			this.Department.Width = 136;
+			// 
+			// ListForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.DeleteButton);
+			this.Controls.Add(this.UpdateButton);
+			this.Controls.Add(this.RegisterButton);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(816, 489);
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(816, 489);
+			this.Name = "ListForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "UserMasterMaintenance";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button RegisterButton;
+		private System.Windows.Forms.Button UpdateButton;
+		private System.Windows.Forms.Button DeleteButton;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewCheckBoxColumn UserCheck;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Gender;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Department;
 	}
 }
 
