bc4b385233a5589d7cdc4ef0324c6d228e9d9385
koda-y <koda-y@evoltech.co.jp>
2020-03-23 17:47:11 +0900

    RunTime.Serializationの追加。項目にDatamenberなどを追加

--- UserMasterMaintenance/Model/Department.cs
+++ UserMasterMaintenance/Model/Department.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace UserMasterMaintenance.Model
 {
+	[DataContract]
 	class Department
 	{
 		/// <summary>
 		/// 部門名
 		/// </summary>
+		[DataMember(Name = "DepartmentName")]
 		public string Name { get; set; } 
 	}
 }
--- UserMasterMaintenance/Model/User.cs
+++ UserMasterMaintenance/Model/User.cs
@@ -3,40 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
-enum Gender
+enum GenderType
 {
+	Nonn,
 	Men,
 	Women
 }
 
 namespace UserMasterMaintenance.Model
 {
-	class User
+	[DataContract]
+	public class User
 	{
 		/// <summary>
 		/// ID
-		/// </summary>
+		/// </summary>]
+		[DataMember(Name = "Id")]
 		public int ID { get; set; }
 
 		/// <summary>
 		/// 名前
 		/// </summary>
+		[DataMember(Name = "Name")]
 		public string Name { get; set; }
 
 		/// <summary>
 		/// 年齢
 		/// </summary>
+		[DataMember(Name = "Age")]
 		public int Age { get; set; }
 
 		/// <summary>
 		/// 性別
 		/// </summary>
-		public Gender Gender { get; set; }
+		[DataMember(Name = "Gender")]
+		public GenderType Gender { get; set; }
 
 		/// <summary>
 		/// 所属部門
 		/// </summary>
+		[DataMember(Name = "Department")]
 		public Department Department { get; set; }
 	}
 }
