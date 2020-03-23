471be2ac109686d6472e20e3ad1a0c56136b9107
koda-y <koda-y@evoltech.co.jp>
2020-03-23 18:06:58 +0900

    修正途中

--- UserMasterMaintenance/Control/JsonFileControl.cs
+++ UserMasterMaintenance/Control/JsonFileControl.cs
@@ -3,26 +3,106 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using Newtonsoft.Json;
+using System.ComponentModel;
 
 namespace UserMasterMaintenance.Control
 {
 	class JsonFileControl
 	{
 		/// <summary>
-		/// ユーザーリスト
+		/// users.jsonのファイルパス
 		/// </summary>
-		public List<Model.User> Users { get; set; }
+		private readonly string UserJsonFilePath = @".\JsonFile\users.json";
 
 		/// <summary>
-		/// 部門リスト
+		/// departments.jsonのファイルパス
 		/// </summary>
-		public List<Model.Department> Departments { get; set; }
+		private readonly string DepartmentJsonFilePath = @".\JsonFile\departments.json";
 
 		/// <summary>
-		/// コンストラクタ
+		/// ユーザーリストを取得する
 		/// </summary>
-		public JsonFileControl()
+		/// <returns></returns>
+		public List<Model.User> GetUsers()
 		{
+			return Deserialize<List<Model.User>>(GetUserJsonText());
+		}
+
+		/// <summary>
+		/// 部門リストを取得する
+		/// </summary>
+		/// <returns></returns>
+		public List<Model.Department> GetDepartments()
+		{
+			return Deserialize<List<Model.Department>>(GetDepartmentJsonText());
+		}
+
+		/// <summary>
+		/// ユーザー情報を保存する
+		/// </summary>
+		/// <param name="users"></param>
+		public void SaveUsersInFile(List<Model.User> users)
+		{
+			using (StreamWriter stream = new StreamWriter(UserJsonFilePath))
+			{
+				stream.Write(users);
+			}
+		}
+
+		/// <summary>
+		/// 部門情報を保存する
+		/// </summary>
+		/// <param name="departments"></param>
+		public void SaveDepartmentsInFile(List<Model.Department> departments)
+		{
+			var jsontext = Selialize(departments);
+			using (StreamWriter stream = new StreamWriter(UserJsonFilePath))
+			{
+				stream.Write(jsontext);
+			}
+		}
+
+		/// <summary>
+		/// ユーザーのjsonテキストを取得する
+		/// </summary>
+		/// <returns></returns>
+		private string GetUserJsonText()
+		{
+			try
+			{
+				using (StreamReader stream = new StreamReader(UserJsonFilePath, Encoding.GetEncoding("shift_jis")))
+				{
+					var jsonFileText = stream.ReadToEnd();
+					return jsonFileText;
+				}
+			}
+			catch (FileNotFoundException)
+			{
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// 部門のjsonテキストを取得する
+		/// </summary>
+		/// <returns></returns>
+		private string GetDepartmentJsonText()
+		{
+			try
+			{
+				using (StreamReader stream = new StreamReader(DepartmentJsonFilePath, Encoding.GetEncoding("shift_jis")))
+				{
+					var jsonFileText = stream.ReadToEnd();
+					return jsonFileText;
+				}
+			}
+			catch (FileNotFoundException)
+			{
+				throw;
+			}
 		}
 
 		/// <summary>
