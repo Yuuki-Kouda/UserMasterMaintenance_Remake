6548f5fb2f7fdb8edde9644916da41ed205edee1
koda-y <koda-y@evoltech.co.jp>
2020-03-23 17:45:20 +0900

    EditControl追加

--- UserMasterMaintenance/Control/EditControl.cs
+++ UserMasterMaintenance/Control/EditControl.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace UserMasterMaintenance.Control
 {
+	public enum EditType
+	{
+		None,
+		Register,
+		Update,
+		Delete
+	}
+
 	class EditControl
 	{
+		/// <summary>
+		/// 編集種類
+		/// </summary>
+		private EditType EditType { get; set; }
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="editType"></param>
+		public EditControl(EditType editType)
+		{
+			EditType = editType;
+		}
+
+		/// <summary>
+		/// 編集する
+		/// </summary>
+		public void DoEdit()
+		{
+			switch (EditType)
+			{
+				case EditType.Register:
+					break;
+				case EditType.Update:
+					break;
+				case EditType.Delete:
+					break;
+				default:
+					break;
+			}
+		}
+
+		private void Register()
+		{
+
+		}
+
+		private void Update()
+		{
+
+		}
+
+		private void Delete()
+		{
+
+		}
 	}
 }
