284aa61a5ed1d9a94f7d4b2f3bd847abeafd4ea0
koda-y <koda-y@evoltech.co.jp>
2020-03-24 17:50:29 +0900

    編集メソッドの追加。登録、更新、削除。　途中

--- UserMasterMaintenance/Control/EditControl.cs
+++ UserMasterMaintenance/Control/EditControl.cs
@@ -38,34 +38,67 @@ namespace UserMasterMaintenance.Control
 		/// <summary>
 		/// 編集する
 		/// </summary>
-		public void DoEdit()
+		public void Edit(Model.User user)
 		{
 			switch (EditType)
 			{
 				case EditType.Register:
+					Register(user);
 					break;
+
 				case EditType.Update:
+					Update(user);
 					break;
+
 				case EditType.Delete:
+					Delete(user);
 					break;
+
 				default:
 					break;
 			}
 		}
 
-		private void Register()
+		/// <summary>
+		/// 登録する
+		/// </summary>
+		/// <param name="user"></param>
+		private void Register(Model.User user)
 		{
-
+			Users.Add(user);
 		}
 
-		private void Update()
+		/// <summary>
+		/// 更新する
+		/// </summary>
+		/// <param name="selectedUser"></param>
+		private void Update(Model.User selectedUser)
 		{
-
+			Users.Where(x => x.ID == selectedUser.ID).Select(x => x = selectedUser).ToList();
 		}
 
-		private void Delete()
+		/// <summary>
+		/// 削除する
+		/// </summary>
+		/// <param name="selectedUser"></param>
+		private void Delete(Model.User selectedUser)
 		{
+			foreach(var user in Users)
+			{
+				if(user.ID == selectedUser.ID)
+					Users[]
+			}
+			Users.re(x => x.ID == selectedUser.ID);
+		}
 
+		/// <summary>
+		/// 重複データがあるかどうか
+		/// </summary>
+		/// <returns></returns>
+		public bool HasDupulicationData(Model.User selectedUser)
+		{
+			//重複すればtrue
+			return Users.Any(x => x.ID == selectedUser.ID);
 		}
 	}
 }
