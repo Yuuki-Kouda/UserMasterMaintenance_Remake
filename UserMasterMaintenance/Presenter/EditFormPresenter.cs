65e917ad33aeaa6936fbb046cf46b46b6d293245
koda-y <koda-y@evoltech.co.jp>
2020-03-30 17:34:26 +0900

    using追加

--- UserMasterMaintenance/Presenter/EditFormPresenter.cs
+++ UserMasterMaintenance/Presenter/EditFormPresenter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
