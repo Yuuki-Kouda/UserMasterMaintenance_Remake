03cb52ff76e304d4b296d36d88dcf35375a3837c
koda-y <koda-y@evoltech.co.jp>
2020-04-03 14:53:31 +0900

    BiginEditをEditUserに修正

--- UserMasterMaintenance/View/EditForm.cs
+++ UserMasterMaintenance/View/EditForm.cs
@@ -48,9 +48,7 @@ namespace UserMasterMaintenance.View
 			if (EditFormPresenter.ConfirmEdit() == DialogResult.Cancel)
 				return;
 
-            // todo : BeginEdti?編集開始？違くない？
-
-			EditFormPresenter.BeginEdit(GetInputItems());
+			EditFormPresenter.EditUser(GetInputItems());
 
 			Close();
 		}
